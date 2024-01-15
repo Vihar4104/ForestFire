31a265f56634a3cef9c5c728e5ae520c06e93fc1
Vihar4104 <tvjcoder414@gmail.com>
2024-01-15 14:55:15 +0530

    commited

--- application.py
+++ application.py
@@ -3,16 +3,17 @@ from flask import Flask, request, jsonify, render_template
 from sklearn.preprocessing import StandardScaler
 
 application = Flask(__name__)
+app = application   
 
 ridge_model = pickle.load(open('model/ridge.pkl', 'rb'))
 scaler_model = pickle.load(open('model/scaler.pkl', 'rb'))
 
 ## Route for Home Page
-@application.route('/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@application.route('/predictdata', methods=['GET','POST'])
+@app.route('/predictdata', methods=['GET','POST'])
 def predict_datapoint():
     if request.method == 'POST':
         Temperature = float(request.form.get('Temperature'))
@@ -37,4 +38,4 @@ def predict_datapoint():
         return render_template('home.html')
 
 if __name__ == "__main__":
-    application.run(host="0.0.0.0")
+    app.run(host="0.0.0.0")
